b2fb18f6b7350577ae92321775babfdafd2cfae1
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-04 11:59:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,5 +11,11 @@ end
 
 def countdown_with_sleep(x)
   x = x
-  while x > 5
-    
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep 1
+  end
+  sleep 5
+  return "HAPPY NEW YEAR!"
+end
