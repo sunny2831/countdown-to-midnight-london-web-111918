3bab4d07b6272ca67fe63dc525c9f9884f4ec2fd
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-04 11:40:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
-end
+def countdown(num_secs)
+  num_secs = 10
+  while num_sec > 0
+    puts "#{num_secs} SECOND(S)!"
+    num_secs -= 1
+    sleep 1
+  end
+    
+end 
